4a4851c6263f21ed9dee57666c03a3c3d8ff4114
sbansal3096 <sbansal3096@gmail.com>
2018-10-25 14:41:49 +0530

    button, intents

--- app/src/main/java/com/example/shubham/audio/MainActivity.java
+++ app/src/main/java/com/example/shubham/audio/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.shubham.audio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText message;
+    EditText key;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button shareButton = (Button) findViewById(R.id.share_button);
+        Button importButton = (Button) findViewById(R.id.import_button);
+        Button encodeButton = (Button) findViewById(R.id.encode);
     }
 }
